fa57db47b1df38e520885733e1c2ffa7d69b1f8b
Gowthami <gowthami.siddi@nooor.in>
2018-08-24 16:57:22 +0530

    default per_page

--- app/src/main/java/com/siddi/imagedownload/Constants.java
+++ app/src/main/java/com/siddi/imagedownload/Constants.java
@@ -6,5 +6,5 @@ package com.siddi.imagedownload;
 
 public class Constants {
 
-    public static String ImageRequest = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736& format=json&nojsoncallback=1&safe_search=1&text=";
+    public static String ImageRequest = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736& format=json&nojsoncallback=1&safe_search=1&per_page=500&text=";
 }
